d3151bf45df3505d6d49c1e7856e41a02aef4484
yakupmutlu <151573226+yakupmutlu@users.noreply.github.com>
2024-01-29 01:24:47 +0300

    Meram-Tarihi-Yerler
    React.js Kütüphaneleri kullanılarak geliştirilmiştir

---
+++ meram-historical-place/src/data.js
@@ -0,0 +1,28 @@
+import "./indir.jpg"
+const reviews = [
+    {
+        id:1,
+        name:"Sahibata",
+        job:"Cami",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Sahib_ata_cami.jpg/330px-Sahib_ata_cami.jpg",
+        text:"Külliye Cami , kıble duvarı arkasında bulunan türbe ve medrese ile çifte hamamdan oluşmaktadır. Külliye 1258 yılında inşa edilmeye başlanmıştır ve eklenen yapılarla 1283 yılında tamamlanmıştır. Cami kısmı 1570 ,1825, 1848,1871 ve son olarak 2007 yılında ise taç kapısı ile beraber restore edilmiştir."
+    },
+    {
+        id:2,
+        name:"Akyokuş Tabiat Parkı",
+        job:"",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Akyokus_parki.jpg/375px-Akyokus_parki.jpg",
+        text:"Yaklaşık 21,6 hektar alana yayılmıştır. Dut, ters dut, badem, huş ağacı, karaçam, dişbudak gibi birçok bitki türünün görülebileceği tabiat parkında sivri fare, tarla faresi, domuz, tavşan, sincap, yılan, kertenkele gibi çok sayıda canlıya rastlamak mümkündür."
+    },
+    {
+        id:3,
+        name:"Konya Etnografya Müzesi",
+        job:"",
+        image:"https://muze.gov.tr/s3/SectionPicture/SP_12ec9277-6c9d-44ab-80f8-7bad768d98a7.jpg",
+        text:"Üç katlı binanın bodrum katında fotoğrafhane, arşiv, ayniyat ve etütlük eser depoları, kaloriferhane ile hâlen çalışmaları devam etmekte olan 1998 yılı içerisinde açılması planlanan halı-kilim seksiyonu bulunmaktadır. Zemin katta teşhir salonu ve Dr. Mehmet Önder Konferans Salonu; birinci katta bürolar, idari hizmet servisleri, kütüphane ve eser depoları bulunmaktadır. Teşhir salonunda satın alma, hediye ve başka müzelerden devir yolu ile müzeye kazandırılan daha çok Konya ve çevresine ait etnografik eserler sergilenmektedir."
+    },
+
+
+]
+
+export default reviews;
